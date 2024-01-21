54eb49dd46f8336e3e01318b46ab0c8441a8881f
Danilo Angel Tito Rodriguez <danilot390@gmail.com>
2024-01-21 06:34:24 +0000

    config initial DB

--- trad/main/admin.py
+++ trad/main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import AboutMe, Contact, New, SocialMedia, Service
 
-# Register your models here.
+@admin.register(AboutMe)
+class AboutmeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(New)
+class NewAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SocialMedia)
+class SocialmediaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
--- trad/main/models.py
+++ trad/main/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AboutMe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    skills = models.CharField(max_length=255)
+    experience = models.TextField()
+
+    def __str__(self):
+        return f'About Me @{self.user.username}'
+
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=30)
+    location = models.CharField(max_length=250, default = '')
+
+    def __str__(self):
+        return f'Contact Me @{self.user.username}'
+
+class SocialMedia(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    social_media = models.CharField(max_length = 50)
+    slug = models.CharField(max_length = 240)
+    url = models.CharField(max_length = 250)
+
+    def __str__(self):
+        return f'{self.social_media} @{self.user.username}'
+    
+class Service(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.CharField(max_length=150)
+    slug = models.CharField(max_length=60)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.service} @{self.user.username}'
+    
+class New(models.Model):
+    title = models.CharField(max_length = 150)
+    description = models.TextField()
+    ranking = models.IntegerField()
+    hidden = models.BooleanField(default = False)
+
+
+    def __str__(self) -> str:
+        return f'New: {self.title}/{self.hidden}'
