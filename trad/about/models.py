f8265a6cc2167a996a826a66ed8b10538e4f7003
danilot390 <71987540+danilot390@users.noreply.github.com>
2024-03-11 19:18:47 +0000

    Update models.py
    Change default employee.img for employee/none.jpg

--- trad/about/models.py
+++ trad/about/models.py
@@ -58,7 +58,7 @@ class Employee(models.Model):
     duties = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(default='none.jpg', upload_to=image_path)
+    image = models.ImageField(default='employee/none.jpg', upload_to=image_path)
 
     def __str__(self):
         return f'{self.user.username} - {self.position}'
